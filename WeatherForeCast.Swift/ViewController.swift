76bc14eb6a2564eb1842e00dcee4e6da06fb7898
李宇佳 <liyujia@liyujiademacbook-pro.local>
2016-06-05 11:33:32 +0800

    Initial Commit

---
+++ WeatherForeCast.Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForeCast.Swift
+//
+//  Created by 李宇佳 on 16/6/5.
+//  Copyright © 2016年 李宇佳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
